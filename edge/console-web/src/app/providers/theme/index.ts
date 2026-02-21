a550e458b094c6ad510257fb728e9a3f15b80f58
Nykon Hrytsyshyn <contact@nykon.me>
2026-02-21 14:21:28 +0200

    Prepare /app for FSD

---
+++ edge/console-web/src/app/providers/theme/index.ts
@@ -0,0 +1,2 @@
+export { ThemeProviderContext } from "./theme-context";
+export { ThemeProvider } from "./theme-provider";
--- edge/console-web/src/index.tsx
+++ edge/console-web/src/index.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 
+import App from "@/app";
 import { createRoot } from "react-dom/client";
 
 import "@app/styles/index.css";
-
-import App from "./app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
